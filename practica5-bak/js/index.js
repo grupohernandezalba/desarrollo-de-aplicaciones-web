7f365aa9cc582a206c18227c360739cf613a7409
Marco Antonio Hernández Alba <marco@grupohernandezalba.com>
2023-02-19 13:06:54 -0600

    1ra parte de la practica5

--- practica5-bak/js/index.js
+++ practica5-bak/js/index.js
@@ -1,34 +1,43 @@
+
     let formulario = document.getElementById("formulario");
     let nombre = document.getElementById("nombre");
     let fecha = document.getElementById("fecha");
     let descripcion = document.getElementById("descripcion");
     let btnGuardar = document.getElementById("btnGuardar");
-    let listaTareas = document.getElementById("listaTareas");
+
     let tareas = [];
 
     let agregarDatos = () => {
         tareas.push({
             nombre: nombre.value,
             fecha: fecha.value,
-            descripcion: descripcion.value
+            descripcion: descripcion.value,
         });
         console.log(tareas);
-    }
+
+    };
+
+    let resetearFormulario = () => {
+        formulario.reset();
+    };
 
     let cerrarModal = () => {
-        btnGuardar.setAttribute("data-bs-dismiss","modal");
+        btnGuardar.setAttribute("data-bs-dismiss", "modal");
         btnGuardar.click();
     }
 
-    let resetarFormulario = () => {
-        nombre.value = '';
-        fecha.value = '';
-        descripcion.value = '';
+    let borrarTarea = (tarea, indice) => {
+        if (confirm("¿Estás seguro de eliminar este registro?")) 
+        {
+            tarea.parentElement.parentElement.remove();
+            tareas.splice(indice, 1);
+        }
     }
 
     let mostrarTareas = () => {
         listaTareas.innerHTML = "";
-        tareas.forEach((tarea,indice)=>{
+
+        tareas.forEach( (tarea, indice) =>{
             listaTareas.innerHTML += `
             <div class='row' id=${indice}>
                 <div class='col-3 border p-3'>
@@ -41,9 +50,7 @@
                     <strong>${tarea.descripcion}</strong>
                 </div>
                 <div class='col-3 border p-3 text-center'>
-                    <button class='btn btn-danger' 
-                    onClick ="borrarTarea(this,${indice});">
-                    <i class="bi bi-trash"></i> Borrar</button>
+                    <button class='btn btn-danger' onClick ="borrarTarea(this,${indice});"><i class="bi bi-trash"></i> Borrar</button>
                 </div>
             </div>
             `;
@@ -54,7 +61,8 @@
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
         agregarDatos();
-        cerrarModal();
-        resetarFormulario();
+        resetearFormulario();
         mostrarTareas();
+        cerrarModal();
+
     });
