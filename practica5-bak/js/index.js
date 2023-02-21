f52fb5c782ab49b28c64bb9acbb6cc2cd0ae2aad
Marco Antonio Hernández Alba <marco@grupohernandezalba.com>
2023-02-20 21:54:22 -0600

    crud completo

--- practica5-bak/js/index.js
+++ practica5-bak/js/index.js
@@ -3,7 +3,15 @@
     let nombre = document.getElementById("nombre");
     let fecha = document.getElementById("fecha");
     let descripcion = document.getElementById("descripcion");
+
+    let formularioEditar = document.getElementById("formularioEditar");
+    let nombreEditar = document.getElementById("nombreEditar");
+    let fechaEditar = document.getElementById("fechaEditar");
+    let descripcionEditar = document.getElementById("descripcionEditar");
+
     let btnGuardar = document.getElementById("btnGuardar");
+    let btnGuardarEditar = document.getElementById("btnGuardarEditar");
+    let idTarea = document.getElementById("idTarea");
 
     let tareas = [];
 
@@ -26,12 +34,19 @@
         btnGuardar.click();
     }
 
-    let borrarTarea = (tarea, indice) => {
+    let borrarTarea = (boton, indice) => {
         if (confirm("¿Estás seguro de eliminar este registro?")) 
         {
-            tarea.parentElement.parentElement.remove();
+            boton.parentElement.parentElement.remove();
             tareas.splice(indice, 1);
         }
+    }
+
+    let editarTarea = (indice) => {
+        nombreEditar.value = tareas[indice].nombre;
+        fechaEditar.value = tareas[indice].fecha;
+        descripcionEditar.value = tareas[indice].descripcion;
+        idTarea.value = indice;
     }
 
     let mostrarTareas = () => {
@@ -46,10 +61,13 @@
                 <div class='col-3 border p-3'>
                     <strong>${tarea.fecha}</strong>
                 </div>
-                <div class='col-3 border p-3'>
+                <div class='col-2 border p-3'>
                     <strong>${tarea.descripcion}</strong>
                 </div>
-                <div class='col-3 border p-3 text-center'>
+                <div class='col-2 border p-3 text-center'>
+                    <button class='btn btn-success' onClick ="editarTarea(${indice});" data-bs-toggle="modal" data-bs-target="#exampleModalEditar"><i class="bi bi-pencil"></i> Editar</button>
+                </div>
+                <div class='col-2 border p-3 text-center'>
                     <button class='btn btn-danger' onClick ="borrarTarea(this,${indice});"><i class="bi bi-trash"></i> Borrar</button>
                 </div>
             </div>
@@ -57,6 +75,21 @@
         });
 
     }
+
+    let cerrarModalEditar = () => {
+        btnGuardarEditar.setAttribute("data-bs-dismiss", "modal");
+        btnGuardarEditar.click();
+    }
+
+    formularioEditar.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let indice = idTarea.value;
+        tareas[indice].nombre = nombreEditar.value;
+        tareas[indice].fecha = fechaEditar.value;
+        tareas[indice].descripcion = descripcionEditar.value;
+        mostrarTareas();
+        cerrarModalEditar();
+    });
 
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
